5b188c3605a450313f1f3fa7a597c363fdcef76d
Vuk3110 <vuktrific@gmail.com>
2024-03-19 13:03:39 +0100

    initial Commit

---
+++ CodeFirst.Model/Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirst.Model.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public string ChassisNumber { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        [ForeignKey("Engine")]
+        public int Engine_Id { get; set; }
+        public Engine Engine { get; set; }
+    }
+}
---
+++ CodeFirst.Model/Models/Engine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst.Model.Models
+{
+    public class Engine
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public string SerialNumber { get; set; }
+        public string Type { get; set; }
+
+        public Car Car { get; set; }
+
+    }
+}
